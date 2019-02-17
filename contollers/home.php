9e4f44ef109b88d08a5a14ba53528b74a02f907c
Khutjo29 <30957244+khutjoideal@users.noreply.github.com>
2019-02-17 08:17:39 +0200

    restructure

--- contollers/home.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Home extends Controller{
-    protected function Index(){
-        echo 'HOME/INDEX';
-    }
-}
--- contollers/users.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Users extends Controller{
-    protected function Index(){
-        echo 'USERS/INDEX';
-    }
-}
