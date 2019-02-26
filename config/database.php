aa027231ae62772134115cb1727e178362c8eff2
Khutjo29 <30957244+khutjoideal@users.noreply.github.com>
2019-02-26 02:19:17 +0200

    conn scs

--- config/database.php
+++ config/database.php
@@ -2,48 +2,41 @@
 
 require('setup.php');
 
-class LoginDB extends loginCred{
-        public function dsn_method(){
-                return parent::dsn_method();
-        }
-}
 
-$run_conn = new LoginDB();
+$run_conn = new loginCred();
 
 try {
-        $run_conn->dsn_method();
+        $conn = $run_conn->dsn_method();
+        if ($conn)
+                echo "connected<br/>";
+        else
+                echo "Error in connection<br/>";
+        echo "succesful<br/>";
 
-        $adap = $run_conn->dsn_method()->prepare('CREATE DATABASE IF NOT EXISTS :db');
-        $adap->execute(['db' => $run_conn->dsn_method($db_camagru)]);
-        $run = $adap->fetch();
+        $sql = "CREATE TABLE IF NOT EXISTS users(
+        id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+        user_first VARCHAR(127) NOT NULL,
+        user_last VARCHAR(127) NOT NULL,
+        user_email VARCHAR(127) NOT NULL,
+        user_iden VARCHAR(127) NOT NULL UNIQUE,
+        user_password VARCHAR(255) NOT NULL,
+        u_profile_pic VARCHAR(20000) NOT NULL,
+        register_date DATETIME
+        )";
+        $conn->exec($sql);
+        echo "successful again <br/>";
 
-        $adap = $run_conn->dsn_method()->prepare('USE :db');
-        $adap->execute(['db' => $run_conn->dsn_method($db_camagru)]);
-        $run = $adap->fetch();
-
-        $adap = $run_conn->dsn_method()->prepare("CREATE TABLE IF NOT EXISTS `users`(
-        `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-        `user_first` VARCHAR(127) NOT NULL,
-        `user_last` VARCHAR(127) NOT NULL,
-        `user_email` VARCHAR(127) NOT NULL,
-        `user_id` VARCHAR(127) NOT NULL UNIQUE,
-        `user_password` VARCHAR(255) NOT NULL,
-        `u_profile_pic` VARCHAR(20000) NOT NULL,
-        `register_date` DATETIME CURRENT_TIMESTAMP,
-        );");
-        $adap->execute([]);
-        $run = $adap->fetch();
-
-        $adap = $run_conn->dsn_method()->prepare("CREATE TABLE IF NOT EXISTS `shares`
-        `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-        `user_id` INT NOT NULL,
-        `title` VARCHAR(255),
-        `body` TEXT,
-        `link` VARCHAR(255),
-        `create_date` DATETIME CURRENT_TIMESTAMP,
+        $sql = null;
+         
+        $sql = ("CREATE TABLE IF NOT EXISTS shares (
+        id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+        user_iden INT NOT NULL,
+        title VARCHAR(255),
+        body TEXT,
+        link VARCHAR(255),
+        create_date DATETIME
         )");
-        $adap->execute([]);
-        $run = $adap->fetch();
+        $conn->exec($sql);
 
         echo "DB created successfully";
 } catch (\PDOException $e) {
--- config/setup.php
+++ config/setup.php
@@ -17,15 +17,26 @@ class loginCred{
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
+        ]; 
 
         try {
+            $DB_DSN = "mysql:host=".$this->host.";charset=".$this->charset;
+            $pdo = new \PDO($DB_DSN, $this->DB_USER, $this->DB_PASSWORD, $this->options);
+            echo "success <br/>";
+
+            $adap = $pdo->prepare("CREATE DATABASE IF NOT EXISTS ".$this->db_camagru);
+            $run = $adap->execute();
+            echo "success <br/>";
+
+            $pdo = null;
             $DB_DSN = "mysql:host=".$this->host.";dbname=".$this->db_camagru.";charset=".$this->charset;
             $pdo = new \PDO($DB_DSN, $this->DB_USER, $this->DB_PASSWORD, $this->options);
+            echo "connected to database<br/>";
             return $pdo;
         } catch (\PDOException $e) {
-                echo "Connection failed: ".$e->getMessage();
+            echo "Connection failed: ".$e->getMessage();
         }
     }
 }
+
 ?>
